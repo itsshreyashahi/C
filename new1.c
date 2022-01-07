1ec0bd7a9617ced1fa751ce23fa88e171c9fbca2
Shreya Shahi <87371443+itsshreyashahi@users.noreply.github.com>
2022-01-07 17:38:14 +0530

    Add files via upload

---
+++ new1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("%d", 3+7);
+    return 0;
+}
---
+++ new2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    a = 35 ;
+    b = 7 ;
+    printf("a + b = %d\n" , a + b);
+    printf("a - b = %d\n" , a - b);
+    
+    return 0;
+
+}
