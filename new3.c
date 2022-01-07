2f4de68fc9d4067f6292cd0141d867783358ef94
Shreya Shahi <87371443+itsshreyashahi@users.noreply.github.com>
2022-01-07 17:39:01 +0530

    Add files via upload

---
+++ new3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    printf("Enter number a :");
+    scanf("%d", &a);
+    printf("Enter number b :");
+    scanf("%d", &b);
+    printf("Sum is %d\n", a + b);
+    printf("Diff is %d\n", a - b);
+    printf("multiply is %d\n", a * b);
+    printf("div is %d\n", a/b);
+    return 0;
+}
