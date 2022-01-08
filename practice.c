ae2018e440161b577d5db28cce5a24a4372dcfa1
Shreya Shahi <87371443+itsshreyashahi@users.noreply.github.com>
2022-01-08 20:37:06 +0530

    Add files via upload

---
+++ practice.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    printf("Enter the number :");
+    scanf("%d", &i);
+if (i % 2 == 0)
+{
+    printf("the number is even ");
+
+}
+else {
+    printf("the number is odd");
+}
+return 0;
+}
